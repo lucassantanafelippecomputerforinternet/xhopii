c1d8c2a916178ed55683a897a97fb16ef5c946ad
Lucas Santana Felippe <106406947+lucassantanafelippecomputerforinternet@users.noreply.github.com>
2022-08-30 10:52:15 -0300

    commit2

---
+++ XhopiiProduct-Inicio/xhopii2/script.js
@@ -0,0 +1,34 @@
+function selecionarImgProduto(imgMiniaturaEscolhida){
+    document.getElementById("imgPrincipal").src = imgMiniaturaEscolhida.src;
+}
+
+    function mudarPreco(botaoEscolhido){
+        if(botaoEscolhido.id =="botaoAmarela"){
+            document.getElementById('precoProduto').innerHTML = "R$2000,00";
+            document.getElementById('pecasDisponiveis').innerHTML = "171 disponiveis";
+        }
+        else if(botaoEscolhido.id =="botaoAzul"){
+            document.getElementById('precoProduto').innerHTML = "R$219,90";
+            document.getElementById('pecasDisponiveis').innerHTML = "179 disponiveis";
+        }
+        else if(botaoEscolhido.id =="botaoNomeNumero"){
+            document.getElementById('precoProduto').innerHTML = "R$499,99";
+            document.getElementById('pecasDisponiveis').innerHTML = "170 disponiveis";
+        }
+        else if(botaoEscolhido.id =="botaoAmarela"){
+            document.getElementById('precoProduto').innerHTML = "R$499,99";
+            document.getElementById('pecasDisponiveis').innerHTML = "177 disponiveis";
+        }
+    }
+
+    function mudarTamanho(botaoEscolhido){
+        if(botaoEscolhido.id =="botaoP"){
+            document.getElementById('tamanhoProduto').innerHTML = "Tamanho selecionado: P";
+        }
+        else if(botaoEscolhido.id =="botaoM"){
+            document.getElementById('tamanhoProduto').innerHTML = "Tamanho selecionado: M";
+        }
+        else if(botaoEscolhido.id =="botaoG"){
+            document.getElementById('tamanhoProduto').innerHTML = "Tamanho selecionado: G";
+        }
+    }
